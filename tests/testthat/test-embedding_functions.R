cfc8e4da171e584d6d1c92cf16e75170efdc0729
huidobropedro <huidobropedro@uniovi.es>
2022-01-13 13:56:59 +0100

    Add first examples width embeddings

--- tests/testthat/test-embedding_functions.R
+++ tests/testthat/test-embedding_functions.R
@@ -1,23 +1,41 @@
 test_that("Width", {
+  #a dentro de b
   expect_equal(emb_w(c(.2, .3), c(0, 1)), 1)
+  #a fuera de b
+  expect_equal(emb_w(c(.2, .3), c(0.4, 1)), 0)
 })
 
 test_that("Lukasiewicz", {
-
+  #a dentro de b
+  expect_equal(emb_w(c(.2, .3), c(0, 1)), 1)
+  #a fuera de b
+  expect_equal(emb_w(c(.2, .3), c(0.4, 1)), 0)
 })
 
 test_that("Fodor", {
-
-})
+  #a dentro de b
+  expect_equal(emb_w(c(.2, .3), c(0, 1)), 1)
+  #a fuera de b
+  expect_equal(emb_w(c(.2, .3), c(0.4, 1)), 0)
+  })
 
 test_that("Godel", {
-
+  #a dentro de b
+  expect_equal(emb_w(c(.2, .3), c(0, 1)), 1)
+  #a fuera de b
+  expect_equal(emb_w(c(.2, .3), c(0.4, 1)), 0)
 })
 
 test_that("Goguen", {
-
+  #a dentro de b
+  expect_equal(emb_w(c(.2, .3), c(0, 1)), 1)
+  #a fuera de b
+  expect_equal(emb_w(c(.2, .3), c(0.4, 1)), 0)
 })
 
 test_that("Rescher", {
-
+  #a dentro de b
+  expect_equal(emb_w(c(.2, .3), c(0, 1)), 1)
+  #a fuera de b
+  expect_equal(emb_w(c(.2, .3), c(0.4, 1)), 0)
 })
