c4800a2177012cb9540fc471a095f3bcf53c7a44
Noelia Rico <noeliarico@uniovi.es>
2023-03-10 10:21:05 +0100

    Doing some cleaning...

--- R/emb_knn.R
+++
@@ -1,9 +0,0 @@
-# train <- matrix(runif(18,0,1),ncol=3) # n x n
-# test <- matrix(runif(12,0,1),ncol=3) # n x n
-# m <- Distance_for_KNN_test(test, train)
-#
-
-
-# get_class <- function(data, nn) {
-#
-# }
